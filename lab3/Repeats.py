9dd5bae5db05c0a24928e98a27d238e383945237
KruglovEgor <50698116+KruglovEgor@users.noreply.github.com>
2022-10-19 13:57:16 +0300

    Simplified
    {1, } -> +

--- lab3/Repeats.py
+++ lab3/Repeats.py
@@ -9,4 +9,4 @@ for i in range(5):
     print(re.sub(pattern, lambda x: x.groups()[-1], Tests.test_repeats[i]), "\n")
 
 #one line solution)))
-#print(*["\nInput:\n" + Tests.test_repeats[i] + "\nOutput:\n" + re.sub(re.compile(r'(\b\w+\b)\W+(\b\1\b\W+){1,}'), lambda x: x.groups()[-1], Tests.test_repeats[i]) + " \n" for i in range(5)])
+#print(*["\nInput:\n" + Tests.test_repeats[i] + "\nOutput:\n" + re.sub(re.compile(r'(\b\w+\b)\W+(\b\1\b\W+)+'), lambda x: x.groups()[-1], Tests.test_repeats[i]) + " \n" for i in range(5)])
